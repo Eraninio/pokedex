330abda11dc4605009977c316224cc23d9b1adc2
Eraninio <67857483+Eraninio@users.noreply.github.com>
2020-08-03 22:06:01 +0300

    end

--- main.js
+++ main.js
@@ -2,12 +2,17 @@ const searchField = document.querySelector('#search');
 const searchButton = document.querySelector('#searchButon');
 const err404Img = document.querySelector('#err404img');
 err404Img.hidden = true;
+const para = document.querySelector('p');
 
-searchButton.addEventListener('click', async () => {
-  const pokemonId = searchField.value;
+// const pokemonId = searchField.value;
+
+searchButton.addEventListener('click', () => {
+  func(searchField.value);
+});
+async function func(id) {
   try {
     err404Img.hidden = true;
-    const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+    const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${id}`);
     const chars = {
     pName : data.name,
     pHeight : data.height,
@@ -20,11 +25,14 @@ searchButton.addEventListener('click', async () => {
   } catch (e) {
     console.log(e);
     err404Img.hidden = false;
+    names.innerHTML = '';
+    typediv.innerHTML = '';
+    para.innerHTML = '';
   }
 
   // console.log(`name: ${chars.pName} height: ${chars.pHeight}, weight: ${chars.pWeight}`);
   // console.log(data);
-});
+}
 
 const typediv = document.getElementById('types');
 const names = document.getElementById('names');
@@ -41,11 +49,14 @@ function InsertDataToHTML(chars) {
         const typeName = document.createElement('li');
         typeName.innerHTML = data.pokemon[j].pokemon.name;
         names.append(typeName);
+        typeName.onclick = () => {
+          func(data.pokemon[j].pokemon.name);
+          names.innerHTML = '';
+        }
       }
     }
   }
 
-  const para = document.querySelector('p');
   para.innerText = `name: ${chars.pName}, height: ${chars.pHeight}, weight: ${chars.pWeight}` ;
   const image = document.createElement('img');
   image.src = chars.pSpriteFront;
