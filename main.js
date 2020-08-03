0a81ebb405fd9856f4ac3a450ac1ba863e16fa5e
Eraninio <67857483+Eraninio@users.noreply.github.com>
2020-08-03 16:34:59 +0300

    1

--- main.js
+++ main.js
@@ -4,6 +4,18 @@ const searchButton = document.querySelector('#searchButon');
 searchButton.addEventListener('click', async () => {
   const pokemonId = searchField.value;
   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-  console.log(data);
+  const chars = {
+    pName : data.name,
+    pHeight : data.height,
+    pWeight : data.weight
+  }
+  InjectDataToHTML(chars);
+  // console.log(`name: ${chars.pName} height: ${chars.pHeight}, weight: ${chars.pWeight}`);
+  // console.log(data);
   });
+
+function InjectDataToHTML(chars) {
+  const para = document.querySelector('p');
+  para.innerText = `name: ${chars.pName} height: ${chars.pHeight}, weight: ${chars.pWeight}`;
+}
 
