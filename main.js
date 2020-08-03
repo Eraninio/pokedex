a89aeddd86c2ff4034a2cc747963154412540378
Eraninio <67857483+Eraninio@users.noreply.github.com>
2020-08-03 16:12:10 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const searchPokemon = async (pokemonId = 3) => {
+const searchField = document.querySelector('#search');
+const searchButton = document.querySelector('#searchButon');
+
+searchButton.addEventListener('click', async () => {
+  const pokemonId = searchField.value;
   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-  console.log(data)
-};
-searchPokemon();
+  console.log(data);
+  });
 
